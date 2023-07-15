cbfed5b7812b3be828dbb868d0d5d97cbc81582d
dmitriy <dkaderov95@gmail.com>
2023-07-16 02:27:03 +0500

    Java first kurs IDE Eclipse

---
+++ kurs1/first_kurs_Lab_2/src/first_kurs_Lab_2/Lesson_1.java
@@ -0,0 +1,10 @@
+package first_kurs_Lab_2;
+
+public class Lesson_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Привет из Eclipse");
+	}
+
+}
